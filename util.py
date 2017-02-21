Fixed averaging

util.py
@@ -175,7 +175,8 @@ def find_lane(img, histogram, left_fit=None, right_fit=None):
 
 
 # Takes RGB image
-def pipeline(orig, mtx, dist, src, dst, base_filename, prev_lfit=None, prev_rfit=None, l_acc=None, r_acc=None,
+def pipeline(orig, mtx, dist, src, dst, base_filename,
+             prev_lfit=None, prev_rfit=None, l_acc=None, r_acc=None,
              output_files='output_images', debug=False):
     if debug:
         mpimg.imsave(os.path.join(output_files, base_filename + "_1_orig.jpg"), orig)
@@ -204,20 +205,20 @@ def pipeline(orig, mtx, dist, src, dst, base_filename, prev_lfit=None, prev_rfit
 
     lfit, rfit = find_lane(img, histogram, left_fit=prev_lfit, right_fit=prev_rfit)
 
-    # if l_acc == None:
-    #     l_acc = collections.deque(maxlen=1)
-    # if r_acc == None:
-    #     r_acc = collections.deque(maxlen=1)
-    #
-    # l_acc.append(lfit)
-    # r_acc.append(rfit)
-    #
+    if l_acc == None:
+        l_acc = collections.deque(maxlen=10)
+    if r_acc == None:
+        r_acc = collections.deque(maxlen=10)
+
+    l_acc.append(lfit)
+    r_acc.append(rfit)
+
     # print(np.shape(lfit))
     # print(np.shape(r_acc))
     # print(np.shape(np.sum(l_acc, 0)))
     # print(l_acc)
-    # lfit = np.array(np.sum(l_acc, 0)) / len(l_acc)
-    # rfit = np.array(np.sum(r_acc, 0)) / len(r_acc)
+    lfit = np.array(np.sum(l_acc, 0)) / len(l_acc)
+    rfit = np.array(np.sum(r_acc, 0)) / len(r_acc)
     # print(lfit)
 
     # Generate x and y values for plotting
@@ -265,4 +266,4 @@ def pipeline(orig, mtx, dist, src, dst, base_filename, prev_lfit=None, prev_rfit
     if debug:
         mpimg.imsave(os.path.join(output_files, base_filename + "_8_output.jpg"), out_img, cmap='gray')
 
-    return out_img, prev_lfit, prev_rfit, l_acc, r_acc
+    return out_img, lfit, rfit, l_acc, r_acc